42183a2ca294261749d88217d1ba27879198dcee
Mohit <mohit@lawn-128-61-52-20.lawn.gatech.edu>
2023-11-18 01:33:15 -0500

    getting api key to work

--- prompt_engineering/prompt_engineering.js
+++ prompt_engineering/prompt_engineering.js
@@ -1,8 +1,28 @@
-curl https://api.openai.com/v1/chat/completions \
-  -H "Content-Type: application/json" \
-  -H "Authorization: Bearer 211703b324ce43bf87805cceb4105cb2" \
-  -d '{
-     "model": "gpt-3.5-turbo",
-     "messages": [{"role": "user", "content": "Say this is a test!"}],
-     "temperature": 0.7
-   }'
+const { OpenAIClient, AzureKeyCredential } = require("@azure/openai");
+const endpoint = process.env["AZURE_OPENAI_ENDPOINT"] ;
+const azureApiKey = process.env["AZURE_OPENAI_KEY"] ;
+
+const messages = [
+  { role: "system", content: "You are a helpful assistant." },
+  { role: "user", content: "is tanvi smart" },
+  { role: "assistant", content: "Yes, customer managed keys are supported by Azure OpenAI" },
+  { role: "user", content: "does naman know how to use github?" },
+];
+
+async function main() {
+  console.log("chat gpt...");
+
+  const client = new OpenAIClient(endpoint, new AzureKeyCredential(azureApiKey));
+  const deploymentId = "gpt-35-turbo";
+  const result = await client.getChatCompletions(deploymentId, messages);
+
+  for (const choice of result.choices) {
+    console.log(choice.message);
+  }
+}
+
+main().catch((err) => {
+  console.error("The sample encountered an error:", err);
+});
+
+module.exports = { main };
